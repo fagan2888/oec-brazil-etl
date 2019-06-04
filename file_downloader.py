11227046abe621ef7f22d837b5a370a2a6b74f86
innerstage <mbarbierif@gmail.com>
2019-06-04 15:49:57 -0400

    Notebooks and files ready

--- file_downloader.py
+++ file_downloader.py
@@ -8,16 +8,16 @@ if "data_temp" not in os.listdir("."):
 flow = sys.argv[1]
 
 def download_file(filename):
-    url = "http://www.mdic.gov.br/balanca/bd/comexstat-bd/ncm/{}".format(filename)
+    url = "http://www.mdic.gov.br/balanca/bd/comexstat-bd/mun/{}".format(filename)
     print("Downloading from {}...".format(url))
-    df = pd.read_csv(url, sep=";")
+    df = pd.read_csv(url, sep=";", encoding="latin-1")
     df.to_csv("./data_temp/{}".format(filename))
     print("Downloaded {}".format(filename))
 
 
 if sys.argv[1] in ["EXP","IMP"]:
     year = sys.argv[2]
-    filename = "{}_{}.csv".format(flow,year)
+    filename = "{}_{}_MUN.csv".format(flow,year)
     if filename in os.listdir("./data_temp"):
         input("The file {} already exists, do you want to replace it? [yes/no]: ".format(filename))
         if input == "yes":
@@ -28,7 +28,7 @@ if sys.argv[1] in ["EXP","IMP"]:
 elif sys.argv[1] == 'all':
     for flow in ["EXP","IMP"]:
         for year in [str(x) for x in range(1997,2020)]:
-            filename = "{}_{}.csv".format(flow,year)
+            filename = "{}_{}_MUN.csv".format(flow,year)
             if filename not in os.listdir("./data_temp"):
                 download_file(filename)
             else:
